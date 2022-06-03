23f952ec99e3f31e62d4da8ff0be32d6ad149d39
Daniel Froes <danielfroestbf@gmail.com>
2022-06-03 16:28:55 -0300

    feat: formulando a page de adotar com grid e mui material

--- pets/src/pages/index.tsx
+++ pets/src/pages/index.tsx
@@ -1,9 +1,30 @@
 import type { NextPage } from 'next'
+import Titulo from '../ui/components/titulo/titulo'
+import Lista from '../ui/components/Lista/Lista'
 
 const Home: NextPage = () => {
   return (
     <div>
+      <Titulo 
+      titulo="" 
+      subtitulo={<span>
+        Com um pequeno valor mensal, você<br/> <strong>pode adotar um pet virtualmente</strong>
+      </span>}/>
 
+      <Lista pets={[
+        {
+          id:1,
+          nome:'Bidu',
+          historia:'sdasjkdashdjaskdasjdhjaks',
+          foto:'https://love.doghero.com.br/wp-content/uploads/2019/09/Dachshund.jpg.jpg'
+        },
+        {
+          id:1,
+          nome:'Scooby',
+          historia:'sdasjkdashdjaskdasjdhjaks',
+          foto:'https://love.doghero.com.br/wp-content/uploads/2019/09/Husky.jpg.jpg'
+        }
+      ]}/>
     </div>
   )
 }
