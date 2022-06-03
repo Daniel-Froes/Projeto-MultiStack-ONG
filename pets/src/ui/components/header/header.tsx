e0a0e6a9ec3897b7f5a382748a59f2d7c6d02fe4
Daniel Froes <danielfroestbf@gmail.com>
2022-06-03 14:39:46 -0300

    feat: arrumando arquivos do next e criando primeiro componente

---
+++ pets/src/ui/components/header/header.tsx
@@ -0,0 +1,10 @@
+import { CabecalhoContainer, Logo } from "./header.style";
+
+export default function Cabecalho(){
+    return(
+        <CabecalhoContainer>
+            <Logo src="/images/logo.svg" alt="Adote um pet"/>
+        </CabecalhoContainer>
+    )
+}
+
