334e271f0c4571a611b189b5384ac5efb6465fd1
Daniel Froes <danielfroestbf@gmail.com>
2022-06-09 22:23:39 -0300

    feat: Arrumando tela de admin do site de pets

--- pets/src/pages/_app.tsx
+++ pets/src/pages/_app.tsx
@@ -3,12 +3,16 @@ import type { AppProps } from 'next/app';
 import {ThemeProvider} from '@mui/material';
 import tema from '../ui/themes/tema';
 import Cabecalho from '../ui/components/header/header';
+import HeaderAdmin from '../ui/components/HeaderAdmin/HeaderAdmin';
+import { useRouter } from 'next/router'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   return (
     <ThemeProvider theme={tema}>
-      <Cabecalho/>
+      {router.pathname === '/' ? <Cabecalho/> : <HeaderAdmin/>}
       <Component {...pageProps} />
     </ThemeProvider>
   ) 
